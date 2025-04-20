519026559593dde26b9bf9863e05838e1c79ba6c
SUDHASNU <123662712+forSUDHANSU@users.noreply.github.com>
2025-04-20 22:35:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let imagebtn=document.querySelector("#image")
 let image=document.querySelector("#image img")
 let imageinput=document.querySelector("#image input")
 
-const Api_Url="https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyCxqzRBugV16rLcgKji5wYP_t6vIQ0_Fdg"
+const Api_Url="add ur gemini api"
 
 let user={
     message:null,
